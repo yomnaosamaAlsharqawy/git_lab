48d286e894c788913b56f7740095f750d11cf905
unknown <ahmedmokhtar55@gmail.com>
2021-02-09 14:54:32 +0200

    js commit

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 console.console.log("yomna osama gaber");
 console.console.log("git lab");
 console.log("lo mejor equipo para siempre"+"the best team forever");
-<<<<<<< HEAD
 console.log("El ahly es el mejor equipo en el mundo");
-=======
 console.log("welcome ahmed");
->>>>>>> 74bb2a8259d071e06780e920ce920136cc86eb21
